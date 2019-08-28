37ff998ee708d9131fce64df38e519b12a46bb21
Chrissy <c.atherholt@gmail.com>
2019-08-28 09:53:01 -0400

    added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,99 @@
-var randomWordArray = [
-    "hearts",
-    "shootingstars",
-    "horseshoes",
-    "clovers",
-    "moons",
-    "rainbows",
-    "redballoons",
-    "potsofgold",
-    "greenhats"
-];
-
-// choose a random word from the array above
-
-function chooseWord() {
-    randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
-    console.log(randomWord);
-}
-
-// some globally defined variables
-var count = 12;
+// declare global variables
+var counter = 0;
 var wins = 0;
 var losses = 0;
-var answerArray = [];
-var guessedArray = [];
+var targetNumber;
 
+var winMsg = "You won!"
+var lossMsg = "You lost :("
+
+// create a start game function that runs when the page loads, and when you win or lose
 function startGame() {
-    chooseWord();
-    answerArray = [];
-    guessedArray = [];
-    count = 12;
-    document.getElementById('wins').innerHTML = wins;
-    document.getElementById('losses').innerHTML = losses;
-    document.getElementById('lettersGuessed').innerHTML = guessedArray;
-    for (var i = 0; i < randomWord.length; i++) {
-        // take the random word and replaces the letters with underscores
-        answerArray[i] = "_";
-    }
-    // put the underscores in a string
-    document.getElementById("answer").innerHTML = answerArray.join(" ");;
+
+    // this empties the crystals ID of its values, which will be reset in a moment
+    $("#crystals").empty();
+
+    // this is an array of the crystal images
+    var images = [
+        "assets/images/BlueGem.png",
+        "assets/images/GreenGem.png",
+        "assets/images/PurpleGem.png",
+        "assets/images/RedGem.png"
+    ];
+
+    // here we determine the random number the player is trying to hit
+    targetNumber = Math.floor(Math.random() * 120) + 19; 
+
+    // then we show that random number in the browser
+    $("#number-to-guess").text(targetNumber);
+
+    // unecessary code that makes the console log easier to read 
+    console.log("-------------");
+    console.log(targetNumber);
+    console.log("-------------");
+
+    // here is the for loop that will create the images, give them backgrounds, and assign them the values we will use in our game
+
+    for (var i = 0; i < 4; i++) {
+
+        // this generates a random number between 1-12 four times that gets assigned to each crystal
+        var randomNum = Math.floor(Math.random() * 11) + 1;
+        console.log(randomNum);
+
+        // here we're creating the images, giving them a class, and giving them the attribute of the random numbers created above 
+        var crystal = $("<img>");
+            crystal.attr({
+            "class": "crystal-image",
+            "data-crystalvalue": randomNum
+        });
+
+        // this assigns the different images above from the array to the backgrounds
+        crystal.css({
+            "background-image" : "url("+images[i]+")"
+        });
+
+        // here we actually add those crystal images with values to the broswer within the crystals ID
+        $("#crystals").append(crystal);
+
+    };
+
+    // update the score with the current counter (should be 0)
+    $("#current-score").text(counter);
+
 }
 
+// run the startGame function when the page loads to set everything up
 startGame();
 
-document.onkeyup = function (event) {
-    console.log(answerArray);
-    // here we get the letter the user types
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+// this is the code that runs everytime you click a crystal
+$(document).on('click', ".crystal-image", function () {
 
-    // check to make sure it's a letter
-    if (/[0-9-_ ]/.test(userGuess)) {
-        alert("That's not a letter");
-    }
-    else {
-        for (var i = 0; i < randomWord.length; i++) {
-            // if the random word contains the userGuessed letter
-            if (randomWord[i] === userGuess) {
-                // assign it to the letter
-                answerArray[i] = userGuess;
-            }
-        }
-        // here we count how many guesses have been made
-        count--;
-        document.getElementById("counter").innerHTML = count;
-        document.getElementById("answer").innerHTML = answerArray.join(" ");
-    }
+    // this code takes the random number assigned to the crystals and converts it into an integer so we can use it
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
 
-    // here's what happens if you WIN
-    if (answerArray.includes("_")) {
+    // now we're adding the values of the crystals to the counter (why we needed integers)
+    counter += crystalValue;
 
-    } else {
+    // update the score everytime we add to it
+    $("#current-score").text(counter);
+
+    // this is code that runs if you WIN
+    if (counter === targetNumber) {
         wins++;
-        count = 12;
-        document.getElementById('counter').innerHTML = count;
+        $("#wins").text(wins);
+        $("#message").text(winMsg);
+        counter = 0;
         startGame();
-        return; // keeps the code further down the page from running
     }
 
-    // here's what happens if you LOSE
-    if (count < 1) {
+    // this is code that runs if you LOSE
+    else if (counter >= targetNumber) {
         losses++;
-        count = 12;
-        document.getElementById('counter').innerHTML = count;
+        $("#losses").text(losses);
+        $("#message").text(lossMsg);
+        counter = 0;
         startGame();
-        return;
     }
 
-    // here's what happens when you guess letters
-
-    // this makes sure letters arent entered in the array twice
-    if (guessedArray.indexOf(userGuess) >= 0) {
-
-    } 
-    // this adds the letters to the guessed letters array
-    else if (count >= 0 ) {
-        guessedArray.push(userGuess);
-        document.getElementById('lettersGuessed').innerHTML = guessedArray.join(", ");
-        console.log(guessedArray);
-    }
-
-}
+});
